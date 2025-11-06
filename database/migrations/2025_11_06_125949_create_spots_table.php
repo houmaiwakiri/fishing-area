fa43fa1d2f624359b5e15be2a251c5b69b39c47b
houmaiwakiri <mahomahoma888@gmail.com>
2025-11-06 22:09:26 +0900

    色々

---
+++ database/migrations/2025_11_06_125949_create_spots_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('spots', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->decimal('lat', 10, 6);
+            $table->decimal('lng', 10, 6);
+            $table->foreignId('prefecture_id')->constrained()->onDelete('cascade');
+            $table->unsignedInteger('rank')->default(0);
+            $table->timestamps();
+        });
+    }
+    public function down(): void
+    {
+        Schema::dropIfExists('spots');
+    }
+};
